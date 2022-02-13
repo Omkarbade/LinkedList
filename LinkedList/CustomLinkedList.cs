3a76b1d0b0987cde91b471cb2529165fee37f5d7
Omkarbade <omakrbade3799@gmail.com>
2022-02-14 01:31:12 +0530

    [Omkar] Add linkedlist program

---
+++ LinkedList/CustomLinkedList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class CustomLinkedList
+    {
+        public Node head;
+        //Method to Add Node at Last in Linked List
+        public void AddLastNode(int data)
+        {
+            Node node = new Node(data);
+            //condition to Check head and add first Node
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                //Temp Node to work
+                Node temp = head;
+                //Looping 
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+        }
+        //Display Method
+        public void DisplayLinkedList()
+        {
+            Console.WriteLine("Display nodes of Linked List");
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.WriteLine("Nodes are: " + temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+        //Method to Add Node at First in Linked List
+        public void AddFirstNode(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+            Console.WriteLine("{0} : Nodes inserted in Linked list ", newNode.data);
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.LinkedListCall();
+            Console.ReadLine();
+        }
+        public void LinkedListCall()
+        {
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.AddLastNode(56);
+            customLinkedList.AddLastNode(30);
+            customLinkedList.AddLastNode(70);
+            Console.WriteLine("nodes inserted");
+            customLinkedList.DisplayLinkedList();
+
+            customLinkedList.AddFirstNode(70);
+            customLinkedList.AddFirstNode(30);
+            customLinkedList.AddFirstNode(56);
+            customLinkedList.DisplayLinkedList();
+        }
+    }
+}
