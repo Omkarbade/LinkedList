f444743fa68ef1c41c1963d963dd57e961c70fe6
Omkarbade <omakrbade3799@gmail.com>
2022-02-14 01:26:38 +0530

    [Omkar] Add linkedlist program

---
+++ LinkedList/CustomLinkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class CustomLinkedList
+    {
+        public Node head;
+        //Method to Add Node in Linked List
+        public void AddNode(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null) //condition to Check head and add first Node
+            {
+                this.head = node;
+            }
+            else
+            {
+                var lastNode = GetLastNode();
+                lastNode.next = node;
+            }
+            Console.WriteLine("Inserted into node: " + node.data);
+        }
+        public Node GetLastNode()
+        {
+            Node temp = head;                                  //Temp Node to work
+            while (temp.next != null)        //Loop to check next pointer in Node   
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.AddNode(56);
+            customLinkedList.AddNode(30);
+            customLinkedList.AddNode(70);
+            Console.ReadLine();
+        }
+    }
+}
