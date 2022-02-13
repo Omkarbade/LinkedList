b16cd98ba655c8ebcb68f2c6716447958b569a44
Omkarbade <omakrbade3799@gmail.com>
2022-02-14 02:37:32 +0530

    [Omkar] Add linkedlist program

---
+++ LinkedList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.LinkedListCall();
+            Console.ReadLine();
+        }
+
+        public void LinkedListCall()
+        {
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.AppendLinkedList(56);
+            customLinkedList.AppendLinkedList(30);
+            customLinkedList.AppendLinkedList(70);
+            customLinkedList.DisplayLinkedList();
+        }
+    }
+}
