6dc6953eb6c8b221504ed19170d7d01ea357c702
Omkarbade <omakrbade3799@gmail.com>
2022-02-15 01:34:36 +0530

    [Omkar] Add linkedlist program

---
+++ LinkedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.LinkedListCall();
+            Console.ReadLine();
+        }
+
+        public void LinkedListCall()
+        {
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.AddLastNode(56);
+            customLinkedList.AddLastNode(70);
+            customLinkedList.DisplayLinkedList();
+            Console.WriteLine("---------------");
+            customLinkedList.AddingDataAtDesiredPosition(1, 30);
+            customLinkedList.DisplayLinkedList();
+        }
+    }
+}
