881381eb87af5b9cdb1ea0ffd6a7a8df9b3b09bd
Omkarbade <omakrbade3799@gmail.com>
2022-02-15 01:53:39 +0530

    [Omkar] Add linkedlist program

---
+++ LinkedList/CustomLinkedList.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class CustomLinkedList
+    {
+        public Node head;
+        //Method to Add Node at Last in Linked List
+        public void AddLastNode(int data)
+        {
+            Node node = new Node(data);
+            //condition to Check head and add first Node
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                //Temp Node to work
+                Node temp = head;
+                
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+        }
+            //Display Method
+        public void DisplayLinkedList()
+        {
+            Console.WriteLine("Display nodes of Linked List");
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.WriteLine("Nodes are: " + temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+
+        public void AddFirstNode(int data)  //Method to Add Node at First in Linked List
+        {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+            Console.WriteLine("{0} : Nodes inserted in Linked list ", newNode.data);
+        }
+
+        public void AppendLinkedList(int data)  //Method to Append Linked List
+        {
+            AddLastNode(data);
+            Console.WriteLine("{0} node Appended", data);
+        }
+        //Method to add Data in middle position
+        public Node AddingDataAtDesiredPosition(int position, int data)
+        {
+            Node newestNode = new Node(data);
+            if (this.head == null)
+            {
+                return newestNode;
+            }
+            //Node Exchange
+            Node prev = null;
+            Node current = this.head;
+            int count = 0;
+            while (current != null && count < position)
+            {
+                prev = current;
+                current = current.next;
+                count++;
+            }
+            newestNode.next = prev.next;
+            prev.next = newestNode;
+            return this.head;
+        }
+        //Method To delete first Node
+        public int DeleteFirstNode()
+        {
+            if (this.head == null)
+            {
+                return 0;
+            }
+            int deleteNode = this.head.data;
+            this.head = this.head.next;
+            return deleteNode;
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.LinkedListCall();
+            Console.ReadLine();
+        }
+
+        public void LinkedListCall()
+        {
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.AddLastNode(56);
+            customLinkedList.AddLastNode(30);
+            customLinkedList.AddLastNode(70);
+            customLinkedList.DisplayLinkedList();
+            Console.WriteLine("---------------");
+            int dnode = customLinkedList.DeleteFirstNode();
+            Console.WriteLine("{0} node deleted", dnode);
+            customLinkedList.DisplayLinkedList();
+        }
+    }
+}
